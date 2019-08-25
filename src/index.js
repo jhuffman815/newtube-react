421f75b3c1ef4e49e193121437d1e747ec18d778
Jewell <jhuffman815@gmail.com>
2019-08-25 15:43:23 -0400

    Created Redux store

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css';
+import { Provider } from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
+import {configureStore} from './store/configureStore';
 
+const store = configureStore();
 
 
 ReactDOM.render(
+   <Provider store={store}>
     <BrowserRouter>
       <App/>
-    </BrowserRouter>, document.getElementById('root'));
+    </BrowserRouter>
+    </Provider>, document.getElementById('root'));
 serviceWorker.unregister();
