e17f744653b1da3b1b11ff09dd07378de0c2f524
Jewell <jhuffman815@gmail.com>
2019-08-25 23:10:44 -0400

    Fetch data for Home page

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,6 +1,22 @@
-import apiReducer from './api';
-import {combineReducers} from 'redux';
-
-export default combineReducers({
-  api: apiReducer,
-});
+import {createSelector} from 'reselect';
+export default function (state = {}, action) {
+  switch (action.type) {
+    case UPDATE_FIRST_NAME:
+      return {
+        ...state,
+        firstName: action.firstName
+      };
+     case UPDATE_LAST_NAME:
+      return {
+        ...state,
+        firstName: action.lastName
+      };
+    default:
+      return state;
+  }
+}
+export const getFullName = createSelector(
+  (state) => state.firstName,
+  (state) => action.lastName,
+  (firstName, lastName) => [firstName,lastName].join(' ')
+);
