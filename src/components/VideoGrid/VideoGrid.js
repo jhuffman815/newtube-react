5235a38d0b4c3df3b6b8b104b39f2163a3e352a1
Jewell <jhuffman815@gmail.com>
2019-08-26 19:32:07 -0400

    Populate Home Feed with Most Popular Vids

--- src/components/VideoGrid/VideoGrid.js
+++ src/components/VideoGrid/VideoGrid.js
@@ -5,26 +5,21 @@ import {Divider} from "semantic-ui-react";
 import {VideoPreview} from '../VideoPreview/VideoPreview';
 
 export function VideoGrid(props) {
+  if (!props.videos || !props.videos.length) {
+    return <div/>;
+  }
+  const gridItems = props.videos.map(video => {
+    return (<VideoPreview video={video}
+                          key={video.id}/>);
+  });
+
   const divider = props.hideDivider ? null : <Divider/>;
   return (
     <React.Fragment>
-    <div className='video-section'>
-        <VideoGridHeader title='Trending' />
+      <VideoGridHeader title={props.title}/>
       <div className='video-grid'>
-        <VideoPreview/>
-        <VideoPreview/>
-        <VideoPreview/>
-        <VideoPreview/>
-        <VideoPreview/>
-        <VideoPreview/>
-        <VideoPreview/>
-        <VideoPreview/>
-        <VideoPreview/>
-        <VideoPreview/>
-        <VideoPreview/>
-        <VideoPreview/>
+        {gridItems}
       </div>
-    </div>
       {divider}
     </React.Fragment>
   );
